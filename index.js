1e39de0372418cb80c17dd99cdd6b760cfa1f872
Channel-Kou <kou@channel.io>
2020-09-07 11:55:48 +0900

    Fix index.js for applying v8 changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import {
   Platform,
   NativeEventEmitter,
   DeviceEventEmitter,
-} from 'react-native';
+} from "react-native";
 
 const ChannelModule = NativeModules.RNChannelIO;
 export const ChannelEventEmitter = Platform.select({
@@ -21,7 +21,7 @@ resetListeners = () => {
   });
 
   listeners = {};
-}
+};
 
 replaceListener = (type, newSubscriber) => {
   let subscriber = listeners[type];
@@ -29,69 +29,97 @@ replaceListener = (type, newSubscriber) => {
     subscriber.remove();
   }
   listeners[type] = newSubscriber;
-}
+};
 
 export const ChannelIO = {
-
   /**
    * Boot `ChannelIO`
    * Note that in order to use any methods from `ChannelIO`, you have to use this method beforehand
-   * @param settings ChannelPluginSettings object contains information for booting
+   * @param bootConfig BootConfig object contains information for booting
    * @returns A promise that returns status and guest info
    */
-  boot: async (settings) => {
-    ChannelModule.setLinkHandle(false);
+  boot: async (bootConfig) => {
+    ChannelModule.setUrlHandle(false);
+    ChannelModule.setPushNotificationHandle(false);
     resetListeners();
 
-    return ChannelModule.boot(settings);
+    return ChannelModule.boot(bootConfig);
+  },
+
+  /**
+   * Sleep `ChannelIO`
+   */
+  sleep: () => {
+    ChannelModule.sleep();
   },
 
   /**
    * Shutdown `ChannelIO`
    */
   shutdown: () => {
-    ChannelModule.setLinkHandle(false);
+    ChannelModule.setUrlHandle(false);
+    ChannelModule.setPushNotificationHandle(false);
     resetListeners();
 
     ChannelModule.shutdown();
   },
 
   /**
-   * Initialize push token
-   * @param {String} token a push token
-   */
-  initPushToken: (token) => ChannelModule.initPushToken(token),
-
-  /**
-   * Show `ChannelIO` launcher
+   * @deprecated
+   * Show `ChannelIO` button
    * @param {Boolean} aniamted Animate the launcher if true
    */
-  show: (animated) => ChannelModule.show(animated),
+  show: (animated) => ChannelModule.showChannelButton(),
+  /**
+   * Show `ChannelIO` button
+   */
+  showChannelButton: () => ChannelModule.showChannelButton(),
 
   /**
-   * Hide `ChannelIO` launcher
+   * @deprecated
+   * Hide `ChannelIO` button
    * @param {Boolean} aniamted Animate the launcher if true
    */
-  hide: (animated) => ChannelModule.hide(animated),
+  hide: (animated) => ChannelModule.hideChannelButton(),
+  /**
+   * Hide `ChannelIO` button
+   */
+  hideChannelButton: () => ChannelModule.hideChannelButton(),
 
   /**
+   * @deprecated
    * Open `ChannelIO` messenger
    * @param {Boolean} aniamted Animate messenger if true
    */
-  open: (animated) => ChannelModule.open(animated),
+  open: (animated) => ChannelModule.showMessenger(),
+  /**
+   * Show `ChannelIO` messenger
+   */
+  showMessenger: () => ChannelModule.showMessenger(),
 
   /**
+   * @deprecated
    * Close `ChannelIO` messenger
    * @param {Boolean} Animate messenger if true
    */
-  close: (animated) => ChannelModule.close(animated),
+  close: (animated) => ChannelModule.hideMessenger(),
+  /**
+   * Hide `ChannelIO` messenger
+   */
+  hideMessenger: () => ChannelModule.hideMessenger(),
 
   /**
-   * Open user chat with given chat Id
+   * Open user chat with given chat id
    * @param {String} chatId user chat id
-   * @param {Boolean} animate Animate messenger if true
+   * @param {String} payload auto fill message
    */
-  openChat: (chatId, animated) => ChannelModule.openChat(chatId, animated),
+  openChat: (chatId, payload) => {
+    if (typeof payload === "string") {
+      ChannelModule.openChat(chatId, payload);
+    } else {
+      ChannelModule.openChat(chatId, undefined);
+    }
+  },
 
   /**
    * Send a event
@@ -101,91 +129,276 @@ export const ChannelIO = {
   track: (eventName, properties) => ChannelModule.track(eventName, properties),
 
   /**
+   * Update user
+   * @param userData userData object contains user data information for updating
+   * @returns A promise that returns exception or user info
+   */
+  updateUser: async (userData) => ChannelModule.updateUser(userData),
+
+  /**
+   * Add user tags
+   * @param tags tags object contains tags information for adding
+   * @returns A promise that returns exception or user data
+   */
+  addTags: async (tags) => ChannelModule.addTags(tags),
+
+  /**
+   * Remove user tags
+   * @param tags tags object contains tags information for removing
+   * @returns A promise that returns exception or user data
+   */
+  removeTags: async (tags) => ChannelModule.removeTags(tags),
+
+  /**
+   * Initialize push token
+   * @param {String} token a push token
+   */
+  initPushToken: (token) => ChannelModule.initPushToken(token),
+
+  /**
    * Check whether a push data is for channel
    * @param {Object} userInfo userInfo part from push data
    * @returns {Boolean} true if the userInfo indicates `ChannelIO'`s push, otherwise false
    */
-  isChannelPushNotification: async (userInfo) => ChannelModule.isChannelPushNotification(userInfo),
+  isChannelPushNotification: async (userInfo) =>
+    ChannelModule.isChannelPushNotification(userInfo),
 
   /**
+   * @deprecated
    * Handle `ChannelIO` push notification
    * @param {Object} userInfo userInfo part from push data
    */
-  handlePushNotification: async (userInfo) => ChannelModule.handlePushNotification(userInfo),
+  handlePushNotification: async (userInfo) =>
+    ChannelModule.receivePushNotification(userInfo),
+  /**
+   * Receive `ChannelIO` push notification
+   * @param {Object} userInfo userInfo part from push data
+   */
+  receivePushNotification: async (userInfo) =>
+    ChannelModule.receivePushNotification(userInfo),
 
   /**
+   * Check whether a push data has stored
+   * @returns {Boolean} true if the `ChannelIO'`s push has stored, otherwise false
+   */
+  hasStoredPushNotification: async () =>
+    ChannelModule.hasStoredPushNotification(),
+
+  /**
+   * Open stored ChannelIO'` push notification
+   */
+  openStoredPushNotification: () => ChannelModule.openStoredPushNotification(),
+
+  /**
+   * @deprecated
    * Event listener that triggers when badge count has been changed
    * @param {Function} cb a callback function that takes a integer badge count as parameter
    */
   onChangeBadge: (cb) => {
-    let subscription = ChannelEventEmitter.addListener(ChannelModule.Event.ON_CHANGE_BADGE, (data) => {
-      cb(data.count);
-    });
+    let subscription = ChannelEventEmitter.addListener(
+      ChannelModule.Event.ON_BADGE_CHANGED,
+      (data) => {
+        cb(data.count);
+      }
+    );
 
-    replaceListener(ChannelModule.Event.ON_CHANGE_BADGE, subscription);
+    replaceListener(ChannelModule.Event.ON_BADGE_CHANGED, subscription);
+  },
+  /**
+   * Event listener that triggers when badge count has been changed
+   * @param {Function} cb a callback function that takes a integer badge count as parameter
+   */
+  onBadgeChanged: (cb) => {
+    let subscription = ChannelEventEmitter.addListener(
+      ChannelModule.Event.ON_BADGE_CHANGED,
+      (data) => {
+        cb(data.count);
+      }
+    );
+
+    replaceListener(ChannelModule.Event.ON_BADGE_CHANGED, subscription);
   },
 
   /**
-   * Event listener that triggers when in-app push has been arrived
-   * @param {Function} cb a callback function that takes a object push data as parameter
+   * @deprecated
+   * Event listener that triggers when in-app popup has been arrived
+   * @param {Function} cb a callback function that takes a object popup data as parameter
    */
   onReceivePush: (cb) => {
-    let subscription = ChannelEventEmitter.addListener(ChannelModule.Event.ON_RECEIVE_PUSH, (data) => {
-      cb(data.push);
-    });
+    let subscription = ChannelEventEmitter.addListener(
+      ChannelModule.Event.ON_POPUP_DATA_RECEIVED,
+      (data) => {
+        cb(data.popup);
+      }
+    );
 
-    replaceListener(ChannelModule.Event.ON_RECEIVE_PUSH, subscription);
+    replaceListener(ChannelModule.Event.ON_POPUP_DATA_RECEIVED, subscription);
+  },
+  /**
+   * Event listener that triggers when in-app popup has been arrived
+   * @param {Function} cb a callback function that takes a object popup data as parameter
+   */
+  onPopupDataReceived: (cb) => {
+    let subscription = ChannelEventEmitter.addListener(
+      ChannelModule.Event.ON_POPUP_DATA_RECEIVED,
+      (data) => {
+        cb(data.popup);
+      }
+    );
+
+    replaceListener(ChannelModule.Event.ON_POPUP_DATA_RECEIVED, subscription);
   },
 
   /**
+   * @deprecated
    * Event listener that triggers when a link has been clicked by a user
    * @param {Boolean} handle True if you want to handle a link, otherwise false
-   * @param {Function} cb a callback function that takes a string link as parameter
+   * @param {Function} cb a callback function that takes a string url as parameter
    */
   onClickChatLink: (handle, cb) => {
-    let subscription = ChannelModule.setLinkHandle(handle);
-    ChannelEventEmitter.addListener(ChannelModule.Event.ON_CLICK_CHAT_LINK, (data) => {
-      cb(data.link);
-    });
-
-    replaceListener(ChannelModule.Event.ON_CLICK_CHAT_LINK, subscription);
+    ChannelModule.setUrlHandle(handle);
+    let subscription = ChannelEventEmitter.addListener(
+      ChannelModule.Event.ON_URL_CLICKED,
+      (data) => {
+        cb(data.url);
+      }
+    );
+    replaceListener(ChannelModule.Event.ON_URL_CLICKED, subscription);
+  },
+  /**
+   * Event listener that triggers when a url has been clicked by a user
+   * @param {Boolean} handle True if you want to handle a url, otherwise false
+   * @param {Function} cb a callback function that takes a string url as parameter
+   */
+  onUrlClicked: (handle, cb) => {
+    ChannelModule.setUrlHandle(handle);
+    let subscription = ChannelEventEmitter.addListener(
+      ChannelModule.Event.ON_URL_CLICKED,
+      (data) => {
+        cb(data.url);
+      }
+    );
+    replaceListener(ChannelModule.Event.ON_URL_CLICKED, subscription);
   },
 
   /**
+   * @deprecated
    * Event listener that triggers when guest profile is updated
    * @param {Function} cb a callback function that takes a key, value
    */
   onChangeProfile: (cb) => {
-    let subscription = ChannelEventEmitter.addListener(ChannelModule.Event.ON_CHANGE_PROFILE, (data) => {
-      cb(data.key, data.value);
-    });
-    replaceListener(ChannelModule.Event.ON_CHANGE_PROFILE, subscription);
+    let subscription = ChannelEventEmitter.addListener(
+      ChannelModule.Event.ON_PROFILE_CHANGED,
+      (data) => {
+        cb(data.key, data.value);
+      }
+    );
+    replaceListener(ChannelModule.Event.ON_PROFILE_CHANGED, subscription);
+  },
+  /**
+   * Event listener that triggers when guest profile is updated
+   * @param {Function} cb a callback function that takes a key, value
+   */
+  onProfileChanged: (cb) => {
+    let subscription = ChannelEventEmitter.addListener(
+      ChannelModule.Event.ON_PROFILE_CHANGED,
+      (data) => {
+        cb(data.key, data.value);
+      }
+    );
+    replaceListener(ChannelModule.Event.ON_PROFILE_CHANGED, subscription);
   },
 
   /**
+   * @deprecated
    * Event listener that triggers when `ChannelIO` messenger is about to display
    * @param {Function} cb a callback function
    */
   willShowMessenger: (cb) => {
-    let subscription = ChannelEventEmitter.addListener(ChannelModule.Event.WILL_SHOW_MESSENGER, cb);
-    replaceListener(ChannelModule.Event.WILL_SHOW_MESSENGER, subscription);
+    let subscription = ChannelEventEmitter.addListener(
+      ChannelModule.Event.ON_SHOW_MESSENGER,
+      cb
+    );
+    replaceListener(ChannelModule.Event.ON_SHOW_MESSENGER, subscription);
+  },
+  /**
+   * Event listener that triggers when `ChannelIO` messenger is about to display
+   * @param {Function} cb a callback function
+   */
+  onShowMessenger: (cb) => {
+    let subscription = ChannelEventEmitter.addListener(
+      ChannelModule.Event.ON_SHOW_MESSENGER,
+      cb
+    );
+    replaceListener(ChannelModule.Event.ON_SHOW_MESSENGER, subscription);
   },
 
   /**
+   * @deprecated
    * Event listener that triggers when `ChannelIO` messenger is about to dismiss
    * @param {Function} cb a callback function
    */
   willHideMessenger: (cb) => {
-    let subscription = ChannelEventEmitter.addListener(ChannelModule.Event.WILL_HIDE_MESSENGER, cb);
-    replaceListener(ChannelModule.Event.WILL_HIDE_MESSENGER, subscription);
+    let subscription = ChannelEventEmitter.addListener(
+      ChannelModule.Event.ON_HIDE_MESSENGER,
+      cb
+    );
+    replaceListener(ChannelModule.Event.ON_HIDE_MESSENGER, subscription);
+  },
+  /**
+   * Event listener that triggers when `ChannelIO` messenger is about to dismiss
+   * @param {Function} cb a callback function
+   */
+  onHideMessenger: (cb) => {
+    let subscription = ChannelEventEmitter.addListener(
+      ChannelModule.Event.ON_HIDE_MESSENGER,
+      cb
+    );
+    replaceListener(ChannelModule.Event.ON_HIDE_MESSENGER, subscription);
+  },
+
+  /**
+   * Event listener that triggers when a chat has been created by a user
+   * @param {Function} cb a callback function that takes a string chat id as parameter
+   */
+  onChatCreated: (cb) => {
+    let subscription = ChannelEventEmitter.addListener(
+      ChannelModule.Event.ON_CHAT_CREATED,
+      (data) => {
+        cb(data.chatId);
+      }
+    );
+    replaceListener(ChannelModule.Event.ON_CHAT_CREATED, subscription);
+  },
+
+  /**
+   * Event listener that triggers when a push has been clicked by a user
+   * @param {Boolean} handle True if you want to handle a push, otherwise false
+   * @param {Function} cb a callback function that takes a string chat id as parameter
+   */
+  onPushNotificationClicked: (handle, cb) => {
+    ChannelModule.setPushNotificationHandle(handle);
+    let subscription = ChannelEventEmitter.addListener(
+      ChannelModule.Event.ON_PUSH_NOTIFICATION_CLICKED,
+      (data) => {
+        cb(data.chatId);
+      }
+    );
+    replaceListener(
+      ChannelModule.Event.ON_PUSH_NOTIFICATION_CLICKED,
+      subscription
+    );
   },
 
   Event: {
-    ON_CHANGE_BADGE: ChannelModule.Event.ON_CHANGE_BADGE,
-    ON_RECEIVE_PUSH: ChannelModule.Event.ON_RECEIVE_PUSH,
-    ON_CLICK_CHAT_LINK: ChannelModule.Event.ON_CLICK_CHAT_LINK,
-    ON_CHANGE_PROFILE: ChannelModule.Event.ON_CHANGE_PROFILE,
-    WILL_SHOW_MESSENGER: ChannelModule.Event.WILL_SHOW_MESSENGER,
-    WILL_HIDE_MESSENGER: ChannelModule.Event.WILL_HIDE_MESSENGER
-  }
-}
+    ON_BADGE_CHANGED: ChannelModule.Event.ON_BADGE_CHANGED,
+    ON_PROFILE_CHANGED: ChannelModule.Event.ON_PROFILE_CHANGED,
+    ON_POPUP_DATA_RECEIVED: ChannelModule.Event.ON_POPUP_DATA_RECEIVED,
+    ON_SHOW_MESSENGER: ChannelModule.Event.ON_SHOW_MESSENGER,
+    ON_HIDE_MESSENGER: ChannelModule.Event.ON_HIDE_MESSENGER,
+    ON_CHAT_CREATED: ChannelModule.Event.ON_CHAT_CREATED,
+    ON_URL_CLICKED: ChannelModule.Event.ON_URL_CLICKED,
+    ON_PUSH_NOTIFICATION_CLICKED:
+      ChannelModule.Event.ON_PUSH_NOTIFICATION_CLICKED,
+  },
+};
